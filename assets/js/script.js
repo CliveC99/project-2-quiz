59fe2d576b01be45fc1f86d40e6dea9f2175609e
CliveC99 <cliveconnell@gmail.com>
2023-02-21 22:53:23 +0000

    Add a rules section, style the rules section and make the rules button functional

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,25 @@
 let startButton = document.getElementById("start-btn");
-let question = document.getElementById("question-area")
+let question = document.getElementById("question-area");
+let rules = document.getElementById("rules-info");
+let rulesButton = document.getElementById("rules-btn");
+
 
 startButton.addEventListener("click", startQuiz);
+rulesButton.addEventListener("click", rulesInfo);
 
 
 
 function startQuiz() {
     startButton.classList.add("hide");
-    question.classList.remove("hide")
+    question.classList.remove("hide");
+    rules.classList.add("hide");
+    rulesButton.classList.add("hide");
+}
+
+function rulesInfo() {
+  rulesButton.classList.add("hide");
+  rules.classList.remove("hide");
+  console.log("test");
 }
 
 function nextQuestion() {
