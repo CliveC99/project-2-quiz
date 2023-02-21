4a90e6150ae333adf0e09ab23d08973ff6623276
CliveC99 <cliveconnell@gmail.com>
2023-02-21 23:48:24 +0000

    Add code to script.js to show the first question

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,13 @@
 let startButton = document.getElementById("start-btn");
-let question = document.getElementById("question-area");
+let questionArea = document.getElementById("question-area");
+let questionSpace = document.getElementById("question");
+let answer = document.getElementById("answer-buttons");
 let rules = document.getElementById("rules-info");
 let rulesButton = document.getElementById("rules-btn");
-let nextButton = document.getElementById("next-btn")
+let nextButton = document.getElementById("next-btn");
 
+
+let shuffleQuestions, currentQuestion;
 
 startButton.addEventListener("click", startQuiz);
 rulesButton.addEventListener("click", rulesInfo);
@@ -13,10 +17,13 @@ nextButton.addEventListener("click", nextQuestion);
 
 function startQuiz() {
     startButton.classList.add("hide");
-    question.classList.remove("hide");
+    questionArea.classList.remove("hide");
+    shuffleQuestions = listOfQuestions.sort(() => Math.random() - ".5");
+    currentQuestion = 0;
     rules.classList.add("hide");
     rulesButton.classList.add("hide");
     nextButton.classList.remove("hide");
+    nextQuestion();
 }
 
 function rulesInfo() {
@@ -25,8 +32,25 @@ function rulesInfo() {
 }
 
 function nextQuestion() {
+  showQuestion(shuffleQuestions[currentQuestion]);
+}
+
+function showQuestion(question) {
+  questionSpace.innerText = question.question;
 }
 
 function selectAnswer() {
 
 }
+
+let listOfQuestions = [
+  {
+    question: "What county was Michael D Higgins born in?",
+    answers: [
+      {text: "Limrick", correct: true},
+      {text: "Galway", correct: false},
+      {text: "Clare", correct: false},
+      {text: "Sligo", correct: false}
+    ]
+  }
+]
