eb011892893a72cc8bea18061ad5463e5233b211
CliveC99 <cliveconnell@gmail.com>
2023-02-21 22:56:45 +0000

    Add the next button to show on the question page

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,12 @@ let startButton = document.getElementById("start-btn");
 let question = document.getElementById("question-area");
 let rules = document.getElementById("rules-info");
 let rulesButton = document.getElementById("rules-btn");
+let nextButton = document.getElementById("next-btn")
 
 
 startButton.addEventListener("click", startQuiz);
 rulesButton.addEventListener("click", rulesInfo);
+nextButton.addEventListener("click", nextQuestion);
 
 
 
@@ -14,16 +16,15 @@ function startQuiz() {
     question.classList.remove("hide");
     rules.classList.add("hide");
     rulesButton.classList.add("hide");
+    nextButton.classList.remove("hide");
 }
 
 function rulesInfo() {
   rulesButton.classList.add("hide");
   rules.classList.remove("hide");
-  console.log("test");
 }
 
 function nextQuestion() {
-
 }
 
 function selectAnswer() {
