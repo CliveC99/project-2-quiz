e9829b9d14ee8348517a5b967bae4cad1a1d8cba
CliveC99 <cliveconnell@gmail.com>
2023-03-09 11:01:12 +0000

    Add code to hide the form once the username is verified

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,12 +44,15 @@ function usernameSubmit(e) {
   rulesButton.classList.remove("hide");
   // Prevent reload of form - help from https://stackoverflow.com/questions/73132199/how-to-avoid-page-refresh-when-user-types-in-invalid-form-input
   e.preventDefault();
+  formSection.classList.add("hide")
 } 
 
 function checkUsername(e) {
   if (username !== null && username.value === "")
   {
     alert("Please enter a username!")
+    // Prevent reload of form - help from https://stackoverflow.com/questions/73132199/how-to-avoid-page-refresh-when-user-types-in-invalid-form-input
+  e.preventDefault();
     e.preventDefault();
   }
 }
